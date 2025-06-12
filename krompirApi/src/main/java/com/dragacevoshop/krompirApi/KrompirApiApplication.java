f707fe8e9634d4aaebafff2159db78ad00b72fd5
nikolaradovanov <ovdebeli@gmail.com>
2025-06-13 00:02:08 +0200

    Create project

---
+++ krompirApi/src/main/java/com/dragacevoshop/krompirApi/KrompirApiApplication.java
@@ -0,0 +1,13 @@
+package com.dragacevoshop.krompirApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrompirApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrompirApiApplication.class, args);
+	}
+
+}
---
+++ krompirApi/src/test/java/com/dragacevoshop/krompirApi/KrompirApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragacevoshop.krompirApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrompirApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
